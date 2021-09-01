c17dc3abe47f39bde526cf01ddcb68597181b005
drachen2299 <benlowry55@gmail.com>
2021-09-01 12:04:28 -0600

    continue button is working

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let questionButtons = document.querySelectorAll(".question-btn");
 let mainButton = document.getElementById("play-btn");
-let nextButton = document.getElementsByClassName("next-btn");
+let nextButton = document.querySelectorAll(".next-btn");
 let mainText = document.querySelector(".main-text");
 let hearts = document.getElementsByClassName(".hearts");
 //get a random question from the opentdb api
-let correctAnswer ;
+let correctAnswer;
 async function fetchData() {
   let url = "https://opentdb.com/api.php?amount=1";
 
@@ -24,8 +24,8 @@ async function fetchData() {
       questionButtons[index].innerText = item;
       questionButtons[index]++;
     });
-    
-    checkAnswer()
+
+    checkAnswer();
     console.log(correctAnswer);
   } catch (error) {
     console.log(error);
@@ -34,48 +34,76 @@ async function fetchData() {
 
 let wrongModal = document.getElementById("wrongModal");
 let correctModal = document.getElementById("correctModal");
-let correctModalContent = document.querySelector(".correct-content")
-let wrongModalContent = document.querySelector(".wrong-content")
+let correctModalContent = document.querySelector(".correct-content");
+let wrongModalContent = document.querySelector(".wrong-content");
 const questionHandler = (e) => {
-    console.log(e.target.innerText)
-    console.log(correctAnswer)
-    if (e.target.innerText === correctAnswer) {        
-        console.log("logging the modal ", correctModal);
-        correctModal.style.display = "block";
-        correctModalContent.classList.remove("hide");
-        
-      } else {
-        wrongModal.style.display = "block";
-        wrongModalContent.classList.remove("hide");
-        
-      }}
+  console.log(e.target.innerText);
+  console.log(correctAnswer);
+  if (e.target.innerText === correctAnswer) {
+    console.log("logging the modal ", correctModal);
+    correctModal.style.display = "block";
+    correctModalContent.classList.remove("hide");
+  } else {
+    wrongModal.style.display = "block";
+    wrongModalContent.classList.remove("hide");
+  }
+};
 
-    function checkAnswer() {
-        questionButtons.forEach((e) => {e.removeEventListener("click", questionHandler)})
-        questionButtons.forEach((e) => {e.addEventListener("click", questionHandler)})
+const toggleHandler = () => {
+  if (correctModal.style.display === "block") {
+    console.log("logging modal") 
+    correctModal.style.display = "none";
+    correctModalContent.classList.add("hide");
+    }else {
+      wrongModal.style.display = "none";
+      wrongModalContent.classList.add("none");
     }
-    // let next question button remove styling and display next question
-    // let nextQuestion = (e) => {
-    //   if (correctModal.style.display === "block"){
-    //    add even listener to remove styleing and go to next question
-    //     e.target.addEventListener("click", () => {
-    //       correctModal.style.display = "none";
-    //       correctModalContent.classList.add("hide");
-    //      })
-    //   }else {
-    //     e.target.addEventListener("click", () => {
-    //     same just reveresed
-    //   }
-    // }
+    mainButton.style.display = "none";
+    fetchData()
+}
 
-    function startGame() {
+function checkModal() {
+  console.log("checkmodal running")
+  nextButton.forEach((e) => {
+    e.addEventListener("click", toggleHandler);
+  })
+}
+
+function checkAnswer() {
+  questionButtons.forEach((e) => {
+    e.removeEventListener("click", questionHandler);
+  });
+  questionButtons.forEach((e) => {
+    e.addEventListener("click", questionHandler);
+  });
+}
+console.log(nextButton)
+function startGame() {
   mainButton.addEventListener("click", () => {
     fetchData();
     console.log(questionButtons);
     mainButton.innerText = "Next Question";
-
+    checkModal();
   });
+  
+  //for each button
+  //add event listener
+  //toggle hide on the events target classList
 }
 
 startGame();
 //when clicking the begin button, load the next question
+// let next question button remove styling and display next question
+// let nextQuestion = (e) => {
+//   switch (e.style.display === "block"){
+//    add even listener to remove styleing and go to next question
+//     e.target.addEventListener("click", () => {
+//       correctModal.style.display = "none";
+//       correctModalContent.classList.add("hide");
+//      })
+//   }else {
+//     e.target.addEventListener("click", () => {
+//     same just reveresed
+//   }
+//return(nextQuestion())
+// }
